d6cce3ef3db3b19c80c2df1597f1acd73df9c6e1
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-04-07 15:47:38 +0200

    test projects

--- simple-express/index.js
+++ simple-express/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const port = 3111;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome to a WebContainers app! 🥳');
 });
@@ -15,6 +17,13 @@ app.get('/test', (req, res) => {
 
 app.post('/echo', (req, res) => {
   res.json(req.body);
+});
+
+app.get('/whatever', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.send(
+    `<h1>Hello</h1><img src='https://upload.wikimedia.org/wikipedia/commons/8/8d/LS3_4919_%28cropped%29.jpg'  alt=''/>`
+  );
 });
 
 app.listen(port, () => {
