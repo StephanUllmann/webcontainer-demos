cd568c0898cdc39f7996b949d975ea3dd5847160
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-04-07 16:00:16 +0200

    test projects

--- express-vite/src/app.ts
+++ express-vite/src/app.ts
@@ -6,6 +6,23 @@ app.get('/', (req, res) => {
   res.send('Hello from Vite + Express + TypeScript!');
 });
 
+app.get('/test', (req, res) => {
+  res.json({
+    msg: 'Test route',
+  });
+});
+
+app.post('/echo', (req, res) => {
+  res.json(req.body);
+});
+
+app.get('/whatever', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.send(
+    `<h1>Hello</h1><img src='https://upload.wikimedia.org/wikipedia/commons/8/8d/LS3_4919_%28cropped%29.jpg'  alt=''/>`
+  );
+});
+
 if (import.meta.env.PROD) {
   app.listen(3000, () => {
     console.log('Server is running on port 3000');
