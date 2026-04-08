3e24d67b4ea3aae3cdc2f682416fc50cc95ebf60
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-04-08 16:06:23 +0200

    project update

--- express-vite/src/app.ts
+++ express-vite/src/app.ts
@@ -21,7 +21,7 @@ app.post('/echo', (req, res) => {
 app.get('/whatever', (req, res) => {
   res.header('Content-Type', 'text/html');
   res.send(
-    `<h1>Hello</h1><img src='https://upload.wikimedia.org/wikipedia/commons/8/8d/LS3_4919_%28cropped%29.jpg'  alt=''/>`
+    `<h1>Hello Web!</h1><img width="234" src='https://upload.wikimedia.org/wikipedia/commons/8/8d/LS3_4919_%28cropped%29.jpg' crossorigin="anonymous" alt=''/>`
   );
 });
 
