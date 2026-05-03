093a865ecd877ea42377d0c996a59b28b701aef2
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-05-03 16:20:13 +0200

    python examples

---
+++ some-py/strings.py
@@ -0,0 +1,50 @@
+# This is a line comment
+
+first_name = "Guybrush"
+last_name = "Threepwood"
+
+"""
+This
+comment
+has multiple
+lines
+"""
+
+
+bio = f"""
+  My name is {first_name} {last_name}.
+I am learning Python.
+Programming is awesome!
+Python!
+
+"""
+
+
+print(bio)
+
+#             01
+book_title = "Käpt'n Blaubär"
+print(book_title[1])
+print(book_title[-5])
+
+print(book_title[7:])
+print(book_title[7:11])
+print(book_title[:6])
+
+len(bio)
+print("Python" in bio)
+print("Java" not in bio)
+print(first_name.upper())
+print(last_name.lower())
+print(bio)
+print(bio.strip())
+# print(bio)
+
+input().strip()
+print(bio.replace("Python", "coding"))
+print(bio.strip().replace("\n", " ").split(" "))
+# greeting = 'He said, "Python' + 's great!"'
+greeting = 'He said, "Python\'s great!"'
+print(greeting)
+
+print("first Line\nsecond line\ttabbed")
